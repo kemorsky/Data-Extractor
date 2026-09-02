84ac675586fd6f9f3d90f5eba790eaf2d1bc1ba5
Kamil Romanowicz <kamil.romanowicz95@gmail.com>
2026-09-02 11:43:41 +0200

    fix: vikunja link text not wrapping on mobile

--- frontend/src/ui/components/drawer/drawer.tsx
+++ frontend/src/ui/components/drawer/drawer.tsx
@@ -228,7 +228,7 @@ export default function LocationDrawer() {
                                 {locationByName?.vikunjaLink !== "" ?
                                     (   <li className={styles.ListItem}>
                                             <span className={styles.ListItemText}>Vikunja Link:</span>
-                                            <a  target="_blank" style={{ fontWeight: 600 }} href={locationByName?.vikunjaLink}>
+                                            <a className={styles.ListItemText} target="_blank" style={{ fontWeight: 600 }} href={locationByName?.vikunjaLink}>
                                                 <span className={styles.ListItemText}>
                                                         {locationByName?.vikunjaLink}
                                                 </span>
