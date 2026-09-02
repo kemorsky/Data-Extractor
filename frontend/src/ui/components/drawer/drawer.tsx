b118541b1ca56be161ed4d449c3d6debf4e4d188
Kamil Romanowicz <kamil.romanowicz95@gmail.com>
2026-09-02 15:15:50 +0200

    Update drawer.tsx

--- frontend/src/ui/components/drawer/drawer.tsx
+++ frontend/src/ui/components/drawer/drawer.tsx
@@ -228,7 +228,6 @@ export default function LocationDrawer() {
                                 {locationByName?.vikunjaLink !== "" ?
                                     (   <li className={styles.ListItem}>
                                             <span className={styles.ListItemText}>Vikunja Link:</span>
-                                            
                                             <a className={styles.ListItemLink} target="_blank" style={{ fontWeight: 600 }} href={locationByName?.vikunjaLink}>
                                                 {/* <span className={styles.ListItemText}> */}
                                                         {locationByName?.vikunjaLink}
